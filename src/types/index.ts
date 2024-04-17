c1920fef56d8ce3ab2ce200faac15b81c5ad8d19
Chingis Choybsonov <avatarius93@gmail.com>
2024-04-17 17:18:34 +0300

    документация в readme, основные типа в types/index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,57 @@
+
+interface IProductItem {
+  id: string;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  image: string;
+}
+
+interface IProductItemUI {
+  title: string;
+  description: string;
+  category: string;
+  price: string;
+  image: string;
+}
+type IProductCardUI = Pick<IProductItemUI, 'title' | 'category' | 'price' | 'image'>
+type IBasketItemUI = Pick<IProductItemUI, 'title' | 'price'>;
+
+
+type PaymentMethod = 'cash' | 'online';
+
+interface IDelivery {
+  payment: PaymentMethod;
+  address: string;
+}
+
+interface IContacts {
+  email: string;
+  phone: string;
+}
+
+interface IOrderList {
+  total: number;
+  items: string[];
+}
+
+type IOrder = IDelivery & IContacts & IOrderList;
+
+interface IOrderBuilder {
+  setDelivery(delivery: IDelivery): void;
+  setContacts(contacts: IContacts):void;
+  setOrderList(orderList: IOrderList): void;
+  getResult(): IOrder;
+}
+
+interface IOrderResult {
+  id: string;
+  total: number;
+}
+
+interface IShopApi {
+  getProductList(): Promise<IProductItem[]>;
+  getProductItem(id:string): Promise<IProductItem>;
+  postOrder(order: IOrder): Promise<IOrderResult>;
+}
